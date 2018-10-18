07403d4f3e3f84fcca73e7179b87ad36d4e7d654
WithLin <withlin@yeah.net>
2018-10-18 09:43:18 +0800

    Fix add Logging module

---
+++ src/common/LcnCsharp.Common/Logging/LcnCsharpLogManager.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LcnCsharp.Common.Logging
+{
+    public  static class LcnCsharpLogManager
+    {
+        public static readonly ILoggerFactory LoggerFactory = new LoggerFactory();
+    }
+}
