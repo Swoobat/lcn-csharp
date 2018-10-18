4517246d579d26b195a60982bb4c69698eb4cc34
WithLin <withlin@yeah.net>
2018-10-18 10:03:20 +0800

    Fix CI broken

---
+++ src/tx-manager/LcnCsharp.Manager.Core/Netty/Model/TxGroup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace LcnCsharp.Manager.Core.Netty.Model
+{
+    public class TxGroup
+    {
+        public string GroupId { get; set; }
+
+        public long StartTime { get; set; }
+
+        public long NowTime { get; set; }
+
+        public int State { get; set; }
+
+        public int HasOver { get; set; }
+
+        /**
+         * 补偿请求
+         */
+        public int IsCompensate { get; set; }
+
+
+        /**
+         * 是否强制回滚(1:开启，0:关闭)
+         */
+        public int Rollback { get; set; } = 0;
+
+        public List<TxInfo> Infos;
+
+        public TxGroup()
+        {
+            Infos=new List<TxInfo>();
+        }
+
+
+        public static TxGroup parser(String json)
+        {
+            try
+            {
+                //JSONObject jsonObject = JSONObject.parseObject(json);
+                //TxGroup txGroup = new TxGroup();
+                //txGroup.setGroupId(jsonObject.getString("g"));
+                //txGroup.setStartTime(jsonObject.getLong("st"));
+                //txGroup.setNowTime(jsonObject.getLong("nt"));
+                //txGroup.setState(jsonObject.getInteger("s"));
+                //txGroup.setIsCompensate(jsonObject.getInteger("i"));
+                //txGroup.setRollback(jsonObject.getInteger("r"));
+                //txGroup.setHasOver(jsonObject.getInteger("o"));
+                //JSONArray array = jsonObject.getJSONArray("l");
+                //int length = array.size();
+                //for (int i = 0; i < length; i++)
+                //{
+                //    JSONObject object = array.getJSONObject(i);
+                //    TxInfo info = new TxInfo();
+                //    info.setKid(object.getString("k"));
+                //    info.setChannelAddress(object.getString("ca"));
+                //    info.setNotify(object.getInteger("n"));
+                //    info.setIsGroup(object.getInteger("ig"));
+                //    info.setAddress(object.getString("a"));
+                //    info.setUniqueKey(object.getString("u"));
+
+                //    info.setModel(object.getString("mn"));
+                //    info.setModelIpAddress(object.getString("ip"));
+                //    info.setMethodStr(object.getString("ms"));
+
+                //    txGroup.getList().add(info);
+                //}
+
+                //var jsonObject = JsonConvert.DeserializeObject(json);
+                //return txGroup;
+                return null;
+
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+        }
+    }
+}
---
+++ src/tx-manager/LcnCsharp.Manager.Core/Netty/Model/TxInfo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using DotNetty.Codecs.Json;
+using LcnCsharp.Manager.Core.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LcnCsharp.Manager.Core.Netty.Model
+{
+    public class TxInfo
+    {
+        /**
+     * 任务唯一标示
+     */
+        public string Kid { get; set; }
+
+        /**
+         * 模块管道名称（netty管道名称）
+         */
+        public string ChannelAddress { get; set; }
+        /**
+         * 是否通知成功
+         */
+        public int Notify { get; set; }
+
+        /**
+         * 0 不组合
+         * 1 组合
+         */
+        public int IsGroup { get; set; }
+
+        /**
+         * tm识别标示
+         */
+        public string Address { get; set; }
+
+        /**
+         * tx识别标示
+         */
+        public string UniqueKey { get; set; }
+
+
+        /**
+         * 管道发送数据
+         */
+        public ChannelSender Channel { get; set; }
+
+
+        /**
+         * 业务方法名称
+         */
+        public string MethodStr { get; set; }
+
+        /**
+         * 模块名称
+         */
+        public string Model { get; set; }
+
+        /**
+         * 模块地址
+         */
+        public string ModelIpAddress { get; set; }
+
+        /**
+         * 是否提交（临时数据）
+         */
+        public int IsCommit { get; set; }
+
+
+        public override string ToString()
+        {
+            var jsonObject = new JObject
+            {
+                { "kid",Kid},
+                { "channelAddress",ChannelAddress},
+                { "notify",Notify},
+                { "notify",Notify},
+                { "isGroup",IsGroup},
+                { "address",Address},
+                { "uniqueKey",UniqueKey},
+                { "model",Model},
+                { "modelIpAddress",ModelIpAddress},
+                { "methodStr",MethodStr}
+
+            };
+            return jsonObject.ToString();
+
+        }
+    }
+}
