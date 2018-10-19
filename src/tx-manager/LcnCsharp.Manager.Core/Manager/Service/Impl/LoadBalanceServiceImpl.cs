be779444eab00129f3514eff554413ecae4c1ae5
WithLin <withlin@yeah.net>
2018-10-19 16:08:13 +0800

    Fix Add Compensate  module

--- src/tx-manager/LcnCsharp.Manager.Core/Manager/Service/Impl/LoadBalanceServiceImpl.cs
+++ src/tx-manager/LcnCsharp.Manager.Core/Manager/Service/Impl/LoadBalanceServiceImpl.cs
@@ -1,27 +1,47 @@
-﻿using LcnCsharp.Manager.Core.Model;
+﻿using LcnCsharp.Manager.Core.Config;
+using LcnCsharp.Manager.Core.Model;
+using LcnCsharp.Manager.Core.Redis.Service;
 
 namespace LcnCsharp.Manager.Core.Manager.Service.Impl
 {
     public class LoadBalanceServiceImpl:ILoadBalanceService
     {
+        private readonly IRedisServerService _redisServerService;
+        private readonly ConfigReader _configReader;
         public bool Put(LoadBalanceInfo loadBalanceInfo)
         {
-            throw new System.NotImplementedException();
+            var groupName = GetLoadBalanceGroupName(loadBalanceInfo.GroupId);
+            _redisServerService.SaveLoadBalance(groupName,loadBalanceInfo.Key,loadBalanceInfo.Data);
+            return true;
         }
 
         public LoadBalanceInfo Get(string groupId, string key)
         {
-            throw new System.NotImplementedException();
+            var groupName = GetLoadBalanceGroupName(groupId);
+            var bytes = _redisServerService.GetLoadBalance(groupName, key);
+            if (bytes == null)
+            {
+                return null;
+            }
+
+            LoadBalanceInfo loadBalanceInfo=new LoadBalanceInfo()
+            {
+                GroupId = groupId,
+                Key = key,
+                Data = bytes
+            };
+            return loadBalanceInfo;
         }
 
         public bool Remove(string groupId)
         {
-            throw new System.NotImplementedException();
+            _redisServerService.DeleteKey(GetLoadBalanceGroupName(groupId));
+            return true;
         }
 
         public string GetLoadBalanceGroupName(string groupId)
         {
-            throw new System.NotImplementedException();
+            return _configReader.Key_prefix_loadbalance + groupId;
         }
     }
 }
--- src/tx-manager/LcnCsharp.Manager.Core/Manager/Service/Impl/TxManagerSenderServiceImpl.cs
+++ src/tx-manager/LcnCsharp.Manager.Core/Manager/Service/Impl/TxManagerSenderServiceImpl.cs
@@ -1,9 +1,19 @@
-﻿using LcnCsharp.Manager.Core.Netty.Model;
+﻿using LcnCsharp.Common.Logging;
+using LcnCsharp.Manager.Core.Config;
+using LcnCsharp.Manager.Core.Netty.Model;
+using LcnCsharp.Manager.Core.Redis.Service;
+using Microsoft.Extensions.Logging;
 
 namespace LcnCsharp.Manager.Core.Manager.Service.Impl
 {
     public class TxManagerSenderServiceImpl:ITxManagerSenderService
     {
+        private readonly ILogger _logger =
+            LcnCsharpLogManager.LoggerFactory.CreateLogger(typeof(TxManagerSenderServiceImpl));
+
+        private ITxManagerService txManagerService;
+        private readonly IRedisServerService _redisServerService;
+        private readonly ConfigReader configReader;
         public int Confirm(TxGroup @group)
         {
             throw new System.NotImplementedException();
